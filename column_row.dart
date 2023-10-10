ac353fabd3d18f02f539ee22a9f2afe3d8895752
epul2207 <epuls073@gmail.com>
2023-10-10 18:00:23 +0700

    Belajar Kolom dan Row

---
+++ column_row.dart
@@ -0,0 +1,18 @@
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: const <Widget>[
+    Icon(Icons.share),
+    Icon(Icons.thumb_up),
+    Icon(Icons.thumb_down),
+  ],
+)
+
+Column(
+  children: const <Widget>[
+    Text(
+      'Sebuah Judul',
+      style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+    ),
+    Text('Lorem ipsum dolor sit amet'),
+  ],
+)
